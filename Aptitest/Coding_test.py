673581e8b43fc43ebf652fb821167251a479a012
tpsdevlop <developers@thoughtprocess.co.in>
2025-02-20 09:54:13 +0530

    20-02-25 1

---
+++ Aptitest/Coding_test.py
@@ -0,0 +1,12 @@
+# from datetime import timedelta
+# import json
+# import random
+# from django.http import HttpResponse, JsonResponse
+# from rest_framework.decorators import api_view
+# from Aptitest.models import *
+# from ExskilenceTest.Blob_service import *
+
+# def get_coding_test_questions():
+#     try:
+
+
